7c2d9b6277ae8854ec8ae098f4ad2fb1a060d25c
SrUltraLord <69977852+SrUltraLord@users.noreply.github.com>
2021-07-03 19:36:37 -0500

    Primer commit jajaja salu2

--- app/src/main/java/com/example/clift/MainActivity.java
+++ app/src/main/java/com/example/clift/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // al bword le gustan las tulas
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
