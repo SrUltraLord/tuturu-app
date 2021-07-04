54f6429ea23d7bbbb73ac44f86b6fe4b4856243e
SrUltraLord <69977852+SrUltraLord@users.noreply.github.com>
2021-07-03 20:44:43 -0500

    Primer commit jajaja salu2

--- app/src/main/java/com/example/clift/MainActivity.java
+++ app/src/main/java/com/example/clift/MainActivity.java
@@ -1,16 +1,105 @@
 package com.example.clift;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    // al bword le gustan las tulas
-    // el 10 se la come
+    private FirebaseAuth mAuth;
+    private Button btnLogin;
+    private EditText txtEmail, txtPassword;
+    private TextView txtVRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // UI Components
+        btnLogin = findViewById(R.id.btnLogin);
+        txtEmail = findViewById(R.id.eTEmailAddress);
+        txtPassword = findViewById(R.id.eTPassword);
+        txtVRegister = findViewById(R.id.txtRegister);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = txtEmail.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                login(email, password);
+            }
+        });
+
+        txtVRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToActivity(RegisterActivity.class);
+            }
+        });
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // updateUI;
+    }
+
+    private void login(String email, String password) {
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Completa todos los campos.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            switchToActivity(LoggedInActivity.class);
+                            // updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Usuario o contraseña incorrecto(s).",
+                                    Toast.LENGTH_SHORT).show();
+                            // updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void switchToActivity(Class actividad) {
+        Intent switchActivityIntent = new Intent(this, actividad);
+        startActivity(switchActivityIntent);
+    }
+
 }
