d1a9b895f4a5e1ff14dc4652547f7e0a9cd393a5
Dangely Rosero <jhoandange@gmail.com>
2021-07-08 12:00:48 -0500

    ultra gei

---
+++ app/src/main/java/com/example/clift/ui/student/ui/solicitud/SolicitudViewModel.java
@@ -0,0 +1,7 @@
+package com.example.clift.ui.student.ui.solicitud;
+
+import androidx.lifecycle.ViewModel;
+
+public class SolicitudViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
