43b3f6c38cc198d3976934c281f3d6c16177a21b
SrUltraLord <69977852+SrUltraLord@users.noreply.github.com>
2021-07-07 19:10:28 -0500

    Primera version del Registro

--- app/src/main/java/com/example/clift/RegisterActivity.java
+++ app/src/main/java/com/example/clift/RegisterActivity.java
@@ -3,16 +3,21 @@ package com.example.clift;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ToggleButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    // Holiwis Vargas otako puerko
-    // AHSDKJSAHDKLJSABDKLJSABKDLJBSA
-    //Hola ultralord trolazo
+    private ToggleButton tBStudent, tBTutor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tBStudent   = findViewById(R.id.tBStudent);
+        tBTutor     = findViewById(R.id.tBTutor);
+
+
     }
 }
