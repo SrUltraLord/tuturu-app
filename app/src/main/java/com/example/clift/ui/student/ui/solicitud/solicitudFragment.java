df9e9543ae5b409ac3e8c9885d3fa330827351d2
Dangely Rosero <jhoandange@gmail.com>
2021-07-08 17:45:06 -0500

    ultra gei

--- app/src/main/java/com/example/clift/ui/student/ui/solicitud/solicitudFragment.java
+++ app/src/main/java/com/example/clift/ui/student/ui/solicitud/solicitudFragment.java
@@ -2,21 +2,34 @@ package com.example.clift.ui.student.ui.solicitud;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.clift.R;
+
+import java.util.Calendar;
+
+import static android.content.ContentValues.TAG;
 
 public class solicitudFragment extends Fragment {
 
     private SolicitudViewModel mViewModel;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+    private EditText btnCalendar;
 
     public static solicitudFragment newInstance() {
         return new solicitudFragment();
@@ -25,7 +38,40 @@ public class solicitudFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.solicitud_fragment, container, false);
+        View root = inflater.inflate(R.layout.solicitud_fragment, container, false);
+
+        btnCalendar = root.findViewById(R.id.etPlannedDate);
+
+        btnCalendar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Calendar kal = Calendar.getInstance();
+                int year = kal.get(Calendar.YEAR);
+                int month = kal.get(Calendar.MONTH);
+                int day = kal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(getContext(), android.R.style.Theme_DeviceDefault_Dialog, dateSetListener, year, month, day);
+
+                dialog.show();
+
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month+1;
+                Log.d(TAG, "onDateSet: dd/mm/yyyy" + day + "/" + month + "/" + year);
+
+                String date = day + "/" + month + "/" + year;
+                btnCalendar.setText(date);
+
+            }
+        };
+
+
+        return root;
     }
 
     @Override
@@ -33,6 +79,8 @@ public class solicitudFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(SolicitudViewModel.class);
         // TODO: Use the ViewModel
+
     }
+
 
 }
